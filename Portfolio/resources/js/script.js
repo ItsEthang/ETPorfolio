1742045d90e510f29c310f6fa0b7b1d328277f81
Ethang <52384982+ItsEthang@users.noreply.github.com>
2023-03-13 01:48:20 -0400

    Add files via upload

--- Portfolio/resources/js/script.js
+++ Portfolio/resources/js/script.js
@@ -1,12 +1,26 @@
 // Header Transition Toggle for Mobile
-const icon = document.querySelector("#icon");
-const list = document.querySelector("#nav-list");
+const icon = document.querySelector("#icon")
+const list = document.querySelector("#nav-list")
 
 icon.addEventListener("click", () => {
-    list.classList.toggle("show");
+    list.classList.toggle("show")
 })
 
 list.addEventListener("click", () => {
-    list.classList.toggle("show");
+    list.classList.toggle("show")
 })
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden')
+hiddenElements.forEach((el) => observer.observe(el))
 
